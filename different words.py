61e751c13851ab80d9a1bbd04e7a61010b54f183
Ivan Genik <IVGenik@rambler.ru>
2023-12-14 23:34:42 +0500

    Add files via upload

--- different words.py
+++ different words.py
@@ -1,34 +1,40 @@
 '''
-Дана строка со словами из 5 букв
+Дана строка со словами из 5 букв / A string with 5 letter words is given
 http://russkiyslovar.ru/iz-5-bukv
-Выбрать слова с максимально различающиеся по буквам
+Выбрать слова максимально различающиеся по буквам / Choose words that differ in letters as much as possible
 '''
+# Как исправить иероглифы в терминале PyCharm на русские буквы?
+# Вряемя и язык -> (справа) админстративные языковые параметры -> галочка в "бета-версия: использовать Юникод..."
+# https://qna.habr.com/q/1130386
+
 # from numba import njit
+import inspect
 from copy import deepcopy
 
-cyrillic_lower_letters:str = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+cyrillic_lower_letters:str = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя' # cyrillic chars
 
-with open('Rus_5letters_words.txt','r') as f:
-    s = f.read()
-lst=s.split()
-new_lst:list = []
-# 1-Шаг: слова с неповторющимися символами
-for s1 in lst:
-    l1 = len(s1)
-    l2 = len(set(s1))
-    if l1==l2:
-        new_lst.append(s1)
-        print(s1)
+def input_lst(fn:str)->list:
+    '''
+    ввод списка из файл / input a list from a file
+    '''
+    print('\n',inspect.currentframe().f_code.co_name)
+    with open(fn,'r') as f:
+        s = f.read()
+    lst=s.split()
+    # print(lst)
+    new_lst:list = []
+    # Выбираем слова с неповторющимися символами / Choosing words with non-repeating characters
+    for s1 in lst:
+        l1 = len(s1)
+        l2 = len(set(s1))
+        if l1==l2:
+            new_lst.append(s1)
+            # print(s1)
 
-print(f' {len(lst)=}')
-new_lst_len = len(new_lst)
-print(f' {new_lst_len=}')
-
-# 2-Шаг: слова с максимально различающимися буквами
-# 1 слово берем из 1 элемента списка
-
-diff_words = [new_lst[0]]
-print(diff_words)
+    print(f' {len(lst)=}')
+    new_lst_len = len(new_lst)
+    print(f' {new_lst_len=}')
+    return new_lst
 
 def delete_chr_from_str(cyrillic_lower_letters:str,s:str)->str:
     '''
@@ -59,23 +65,39 @@ def thetest_all_chr_in_str():
 
 
 def find_different_words(cyrillic_lower_letters, new_lst:list[str]):
+    print('\n',inspect.currentframe().f_code.co_name)
     # https://www.cyberforum.ru/python-beginners/thread2695448.html
     s_3:str = ''
     new_lst_len:int = len(new_lst)
-    diff_words:list = [new_lst[0]] # вывбираем первое слово (с неповторяющимися буквами)
-    cyrillic_lower_letters = delete_chr_from_str(cyrillic_lower_letters, new_lst[0]) # Удаляем буквы первого слова
-    print('Big Cycle')
-    for i in range(1,new_lst_len): # перебор по всем словам в общем списке
-        print(i,' from', new_lst_len)
-        if all_chr_in_str(cyrillic_lower_letters,new_lst[i]):
-            diff_words.append(new_lst[i])
-            res_max = 0 # перебор по всем словам в целевом списке
-            cyrillic_lower_letters = delete_chr_from_str(cyrillic_lower_letters,new_lst[i])
+    print(f'{new_lst_len=}')
+    # https://www.geeksforgeeks.org/how-to-create-a-list-of-n-lists-in-python/
+    diff_words:list = [[] for x in range(new_lst_len)]
+    for i in range(0,new_lst_len): # перебор по всем словам в общем списке
+        diff_words[i].append('') # увеличиваем список
+        diff_words[i][0] = new_lst[i]
+        cyrillic_lower_letters_curr = delete_chr_from_str(cyrillic_lower_letters, new_lst[i]) # Удаляем буквы этого слова
+        for j in range(0,new_lst_len): # перебор по всем оставшимся словам в общем списке
+            if i != j:
+                if all_chr_in_str(cyrillic_lower_letters_curr,new_lst[j]):
+                    diff_words[i].append(new_lst[j])
+                    cyrillic_lower_letters_curr = delete_chr_from_str(cyrillic_lower_letters_curr,new_lst[j])
+        print('len(diff_words[',i,'])=',len(diff_words[i]))
+        # print(diff_words[i])
+        # print('Остатки букв = ',cyrillic_lower_letters_curr)
+    return diff_words
 
-    print('len(diff_words)=',len(diff_words))
-    print(diff_words)
-    print('Отстатки букв = ',cyrillic_lower_letters)
+def output_list_of_list_to_file(fn:str, lst:list)->None:
+    print('\n',inspect.currentframe().f_code.co_name)
+    # print(lst)
+    with open(fn,'w') as f:
+        for i,s1 in enumerate(lst):
+            f.write(f'{i:4} ')
+            for s2 in s1:
+                f.write(s2+' ')
+            f.write('\n')
 
-# thetest_all_chr_in_str()
 
-find_different_words(cyrillic_lower_letters, new_lst)
+# ---- Main program
+new_lst = input_lst('Rus_5letters_words.txt')
+diff_words = find_different_words(cyrillic_lower_letters, new_lst)
+output_list_of_list_to_file('different words.txt',diff_words)
