eeb7f626db72a9b7b2b7b9881f658e3e4568b812
Ivan Genik <IVGenik@rambler.ru>
2023-12-28 16:58:24 +0500

    Add files via upload

---
+++ Scrap2.py
@@ -0,0 +1,32 @@
+'''
+A) get/put parameters from json
+https://stackoverflow.com/questions/19078170/python-how-would-you-save-a-simple-settings-config-file
+
+B) Scraping
+08.05.2021 Скачивание файлов в Python АВТОР В. ДРОНОВ
+https://tonais.ru/file/skachivanie-faylov-v-python
+'''
+
+import urllib.request
+from pstring import swlz
+from configparser import ConfigParser
+import sys
+
+def get_params_from_json()-> (str, str):
+    config = ConfigParser()
+    config.read('Scrap2_config.ini')
+    a = config.get('main', 'base_url'); base_url_ = a.strip(); print(f'{base_url_=}')
+    a = config.get('main', 'folder');  folder_ = a.strip(); print(f'{folder_=}')
+    return base_url_, folder_
+
+base_url_, folder_    = get_params_from_json()
+# https://python-lab.ru/vyhod-i-zavershenie-programmy-v-python
+# sys.exit(0)
+for i in range(1,22): # 1,285
+    print(i)
+    fsn = 'page-'+str(i)
+    fdn = 'page-'+ swlz(i, 3)
+    curr_url = base_url_+fsn
+    res_fn = folder_+fdn+'.webp'
+    urllib.request.urlretrieve(curr_url,res_fn)
+
